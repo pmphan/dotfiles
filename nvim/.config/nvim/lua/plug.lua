2bf4c786f91062742e80c81e665f5823bb655922
Phuong <pmphan@gmail.com>
2023-08-15 03:19:48 -0700

    [nvim] Viewer for graphed branches.

--- nvim/.config/nvim/lua/plug.lua
+++ nvim/.config/nvim/lua/plug.lua
@@ -69,6 +69,9 @@ require("lazy").setup({
         }
     },
 
+    -- FLog
+    { "rbong/vim-flog", name = "vim-flog" },
+
     -- Autocomplete/LSP
     { "neoclide/coc.nvim", branch = "release", name = "coc" },
 
