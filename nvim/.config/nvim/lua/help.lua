ed4b97231a8dbd0443af522b8ee0c66fcebebd02
Phuong <pmphan@gmail.com>
2023-08-14 22:39:42 -0700

    [nvim] Gitsigns and commentary.

--- nvim/.config/nvim/lua/help.lua
+++ nvim/.config/nvim/lua/help.lua
@@ -40,4 +40,40 @@ function M.on_attach_nvim_tree(bufnr)
     vim.keymap.set('n', '<C-h>', api.node.open.horizontal, opts('Open: Horizontal Split'))
 end
 
+function M.on_attach_gitsigns(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation between hunks
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Actions
+    map('n', '<Leader>hr', gs.reset_hunk)
+    map('v', '<Leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', '<Leader>hR', gs.reset_buffer)
+
+    map('n', '<Leader>hs', gs.stage_hunk)
+    map('v', '<Leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', '<Leader>hS', gs.stage_buffer)
+
+    map('n', '<Leader>hu', gs.undo_stage_hunk)
+    map('n', '<Leader>hp', gs.preview_hunk)
+    map('n', '<Leader>hd', gs.diffthis)
+end
+
 return M
